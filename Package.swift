8079541de4e5029d09d8a90349653f22c7ea1baa
Ian Partridge <i.partridge@uk.ibm.com>
2017-03-30 11:52:44 +0100

    Create project

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "kitura-helloworld",
+    name: "kitura-staticfiles",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6)
     ]
